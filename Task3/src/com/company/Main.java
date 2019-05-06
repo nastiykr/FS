c7b55cbefce4f9c97601f4bd84af59ac6a20409c
Anastasiya Krivokora <anastasiya.krivokora@CORP.TASKDATA.RU>
2019-05-06 13:22:00 +0300

    task3-1

--- Task3/src/com/company/Main.java
+++ Task3/src/com/company/Main.java
@@ -4,25 +4,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        //int n = 5;
         int n = (int) (Math.random() * 10);
 
-        int[][] arr = new int[n][n];
-        for(int i=0; i< arr.length; i++){
-            for(int j=0;j < arr[i].length; j++){
-                arr[i][j]=(int) (Math.random() * 10);;
-                System.out.print(arr[i][j]+" ");
+        int[][] balances = new int[n][n];
+        for(int i=0; i< balances.length; i++){
+            for(int j=0;j < balances[i].length; j++){
+                balances[i][j]=(int) (Math.random() * 10);;
+                System.out.print(balances[i][j]+" ");
             }
             System.out.println("");
         }
 
         System.out.println("");
 
-        for(int i=0; i< arr.length; i++){
-            for(int j=0;j < arr[i].length; j++) {
-                System.out.print(arr[j][i] + " ");
+        for(int i=0; i< balances.length; i++){
+            for(int j = i + 1;j < n; j++) {
+
+                balances[i][j] = balances[i][j] + balances[j][i];
+                balances[j][i] = balances[i][j] - balances[j][i];
+                balances[i][j] = balances[i][j]- balances[j][i];
+                //System.out.print(arr[i][j] + " ");
             }
-            System.out.println("");
+            //System.out.println("");
+        }
+
+        for (int i = 0; i < balances.length; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(balances[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
