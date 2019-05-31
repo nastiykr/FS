c02a867511954b9ae652c4358ac47fb4e249c57b
Anastasiya Krivokora <anastasiya.krivokora@CORP.TASKDATA.RU>
2019-05-31 13:39:50 +0300

    commit

--- Lecture17/CucumberTestHome/src/main/java/SomeFile.java
+++ Lecture17/CucumberTestHome/src/main/java/SomeFile.java
@@ -1,4 +1,5 @@
 import lombok.Getter;
+import org.junit.Assert;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,21 +12,22 @@ import java.nio.file.Paths;
 public class SomeFile {
 
     private String file;
+    Path directoryPath;
 
     public SomeFile(String path) {
         this.file = path;
     }
 
     public void createFile() {
-        Path directoryPath = Paths.get(this.file);
+        directoryPath = Paths.get(this.file);
         try {
             Files.createDirectory(directoryPath);
         } catch (IOException e) {
         }
 
     }
-    public String getFilePath() {
-
-        return this.file;
+    public boolean getFilePath() {
+        boolean fileExists = Files.exists(directoryPath);
+        return fileExists;
     }
 }
--- Lecture17/CucumberTestHome/src/test/java/SomeFileTest.java
+++ Lecture17/CucumberTestHome/src/test/java/SomeFileTest.java
@@ -3,6 +3,10 @@ import cucumber.api.java.ru.Когда;
 import cucumber.api.java.ru.Тогда;
 import org.junit.Assert;
 import java.io.File;
+import java.nio.file.Files;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 public class SomeFileTest {
 
@@ -16,16 +20,13 @@ public class SomeFileTest {
     @Когда("Пользователь вызывает метод создания filePath")
     public void пользовательВызываетМетодСозданияFilePath( ) {
         TestContext.instance.createFile();
-        System.out.println(" **********************");
         System.out.println("Когда. Файл создан");
     }
 
-    @Тогда("filePath создан, на экран выводится сообщение равное {string}")
-    public void filepathСозданНаЭкранВыводитсяСообщениеРавное() {
-        String targetValue = TestContext.instance.getFilePath();
-
-        Assert.assertEquals("ff", targetValue);
-        System.out.println(" **********************");
+    @Тогда("filePath создан")
+    public void filepathСоздан() {
+        boolean targetValue = TestContext.instance.getFilePath();
+        Assert.assertTrue(targetValue);
         System.out.println("Тогда. Проверка существования файла");
     }
 }
